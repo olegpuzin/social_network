a71f3e61abe782c79cbacbef2715d6955e12b8f1
olegpuzin <olegpuzin77@icloud.com>
2022-11-12 09:54:50 +0200

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import MainLayout from './layouts/MainLayout';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (        
+  <Routes>
+    <Route path='/' element={<MainLayout />}>
+      {/* <Route path='/' element={<Home />}/>
+      <Route path='/cart' element={<Cart />}/>
+      <Route path='/pizza/:id' element={<FullPizza />}/>
+      <Route path='*' element={<NotFound />}/> */}
+    </Route>
+  </Routes>
   );
 }
 
